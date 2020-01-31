11f8c922860c6f182e67cc8085f0c225a8eb7c9f
Ayush <ayush.nasa@gmail.com>
2020-02-01 01:49:42 +0530

    minor

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ def genericRequest():
 def create_todo():
 
     todo = request.args.get('todo')
-    todo = request.args.get('todo1')
     token_v2 = os.environ.get("TOKEN")
     url = os.environ.get("URL")
     createNotionTask(token_v2, url, todo)
