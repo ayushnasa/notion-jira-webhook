c30d63cd2a2cd9bc6f28eba3166a9ddbeb8d0204
Ayush Vashishtha <ayush@Ayushs-MacBook-Pro.local>
2020-08-30 13:27:02 +0530

    Added support for JIRA webhook

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-
 import os
-from notion.client import NotionClient
+import argparse
+import sys
+from datetime import datetime
+
 from flask import Flask
 from flask import request
 
+from notion.client import *
+from notion.block import *
 
 app = Flask(__name__)
 
 
-def createNotionTask(token, collectionURL, content):
-    # notion
-    client = NotionClient(token)
-    cv = client.get_collection_view(collectionURL)
-    row = cv.collection.add_row()
-    row.title = content
 
 # ?fields=status,date,bullshit&values=
 
@@ -24,14 +22,65 @@ def genericRequest():
     print(content.values())
     return { "success": True }
 
-@app.route('/create_todo', methods=['GET'])
-def create_todo():
 
-    todo = request.args.get('todo')
+
+
+
+
+
+@app.route('/createSprintTask', methods=['POST'])
+def createSprintTask():
+    json = request.jsons
     token_v2 = os.environ.get("TOKEN")
-    url = os.environ.get("URL")
-    createNotionTask(token_v2, url, todo)
-    return f'added {todo} to Notion'
+    client = NotionClient(token_v2)
+
+    url = request.args.get('url')
+    cv = client.get_collection_view(collectionURL)
+
+    title = request.args.get('title')
+
+    row = cv.collection.add_row()
+    row.title = title
+    row.jira_link = request.args.get('jira_link')
+    row.status = request.args.get('status')
+    row.priority = request.args.get('priority')
+    row.assignee = request.args.get('assignee')
+    row.dev_owner = request.args.get('dev_owner')
+    row.qa_owner = request.args.get('qa_owner')
+    row.labels = request.args.get('labels')
+
+    return f'Added {title} to Notion'
+
+
+
+
+
+@app.route('/editSprintTask', methods=['POST'])
+def editSprintTask():
+    json = request.json
+    token_v2 = os.environ.get("TOKEN")
+    client = NotionClient(token_v2)
+
+    url = request.args.get('url')
+    cv = client.get_collection_view(collectionURL)
+
+    jiraId = request.args.get('jiraId')
+
+#TODO fix below things
+    assert row in cv.collection.get_rows(search=jiraId)
+    assert row.title = request.args.get('title')
+    assert row.status = request.args.get('status')
+    assert row.priority = request.args.get('priority')
+    assert row.assignee = request.args.get('assignee')
+    assert row.dev_owner = request.args.get('dev_owner')
+    assert row.qa_owner = request.args.get('qa_owner')
+    assert row.labels = request.args.get('labels')# fix above things
+# fix above things
+
+    return f'Edited {jiraId} in Notion'
+
+
+
 
 
 if __name__ == '__main__':
