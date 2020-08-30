91cac9946304e23586c8ad90352e2ea578e31d37
Ayush Vashishtha <ayush@Ayushs-MacBook-Pro.local>
2020-08-30 15:12:20 +0530

    Bug

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def editSprintTask():
                     #             values.append(val)
                     #             row[key] = values
                     # else:
-                    row[key] = json[key]
+                    row.set(key,json[key])
                 except Exception as e:
                     print('Exception!!!!')
                     print(e)
